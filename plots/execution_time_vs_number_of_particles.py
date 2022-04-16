50ac32451ad8141a5343adbe6c001f90e62e1f93
oserpe <oserpe@itba.edu.ar>
2022-04-16 17:36:16 -0300

    :sparkles: add execution time vs number of particles plot

---
+++ plots/execution_time_vs_number_of_particles.py
@@ -0,0 +1,54 @@
+import argparse
+import os
+from statistics import stdev
+
+import matplotlib.pyplot as plot
+
+
+def execution_time_vs_number_of_particles(threshold, repetitions):
+    print(
+        f"Running simulation with {threshold} left particles fraction threshold.")
+    particle_count = [100, 150, 200]
+    average_execution_time = []
+    average_execution_time_stdev = []
+    for total_particles in particle_count:
+        execution_time = []
+        for _ in range(repetitions):
+            cmd = f"java -DnumberOfParticles={total_particles} -DleftParticlesFractionThreshold={threshold} -jar ../target/event-driven-molecular-dynamics-gas-1.0-SNAPSHOT.jar"
+            print(f"Executing: {cmd}")
+            os.system(cmd)
+            print("Done")
+
+            with open("summary.txt") as summary_file:
+                for line_number, line in enumerate(summary_file):
+                    if line_number == 4:
+                        execution_time.append(float(line.split()[0]))
+
+        average_execution_time.append(
+            float(sum(execution_time) / len(execution_time)))
+        average_execution_time_stdev.append(
+            stdev(execution_time) if len(execution_time) > 1 else 0)
+
+    # Plot execution time vs number of particles
+    plot.errorbar(average_execution_time, particle_count, ls="none",
+                  yerr=average_execution_time_stdev, ecolor='blue', marker='o', color="red", elinewidth=0.5, capsize=5)
+
+    plot.xlabel("Number of particles")
+    plot.ylabel("Execution time (s)")
+
+    plot.show()
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--repetitions", default=1,
+                        help="The number of repetitions of the simulation. Defaults to 1.", dest="repetitions", required=False)
+    parser.add_argument("--threshold", default=0.5,
+                        help="The threshold of the left particles fraction used in the simulations. Defaults to 0.5.", dest="threshold", required=False)
+
+    args = parser.parse_args()
+
+    execution_time_vs_number_of_particles(
+        float(args.threshold), int(args.repetitions))
